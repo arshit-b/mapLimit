46d26b6d03f357bc92ac24ac5b2d5178cd069b31
arshit-b <arshitbabariya301@gmail.com>
2024-03-04 11:13:16 +0530

    add problem statement

--- mapLimit.ts
+++ mapLimit.ts
@@ -1,3 +1,14 @@
+// https://leetcode.com/discuss/interview-question/2409192/Uber-or-Phone-Screen-or-Senior-Front-End-Engineer
+// Implement mapLimit, which is a utility function that produces a list of outputs by mapping each input through an asynchronous iteratee function. The provided limit dictates how many operations can occur at once.
+
+// Inputs
+// inputs: An array of inputs.
+// limit: The maximum number of operations at any one time.
+// iterateeFn: The async function that should be called with each input to generate the corresponding output. It will have two arguments:
+//      input: The input being processed.
+//      callback: A function that will be called when the input is finished processing. It will be provided one argument, the processed output.
+// callback: A function that should be called with the array of outputs once all the inputs have been processed.
+
 const mapLimit = (arr, limit, callback) => {
   let index = 0;
   const answer = [];
